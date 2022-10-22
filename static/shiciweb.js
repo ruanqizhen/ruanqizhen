700422c9a7958c3de4dd4715393c851f4ee61874
ruanqizhen <ruanqizhen@gmail.com>
2022-10-22 16:05:13 -0400

    update

--- static/shiciweb.js
+++ static/shiciweb.js
@@ -53,3 +53,44 @@ function writePoem(){
 	setWaitingMessage();
 }
 
+function generateImage(){
+	
+	var jqxhr = $.post(
+		"https://service.qizhen.xyz/genimg/", 
+		$('#input_form').serialize(),
+	).done(function (data) {
+		document.getElementById("result_section").innerHTML = "";
+		var result_div = $('#result_section');
+
+		result_div.html('<img src="data:image/png;base64,' + data + '" />')
+
+	}).fail(function (xhr, status) {
+		document.getElementById("result_section").innerHTML 
+		= "<b>服务器目前无法工作。服务器是我的测试机器，不太稳定。请明天再试试！</b><br /><br />";
+	});
+	document.getElementById("result_section").innerHTML
+    = "程序运行需要几十秒钟，请稍等 ……  <br> <progress style=\"width: 20%; height: 30px\"></progress>";
+}
+
+function checkStatus(){
+	
+	var jqxhr = $.get(
+		"https://service.qizhen.xyz/status/", 
+	).done(function (data) {
+		var anchor_node = $('#model');
+		switch(data) {
+		  case "1":
+			anchor_node.html("目前正在运行“吟诗作对”模型。");
+			break;
+		  case "2":
+			anchor_node.html("目前正在运行“电脑绘图”模型。");
+			break;
+		  default:
+			anchor_node.html("<b>目前服务器无法工作，啥都运行不了！</b>");
+		}
+
+	}).fail(function (xhr, status) {
+		var anchor_node = $('#model');
+		anchor_node.html("<b>目前服务器无法工作，啥都运行不了！</b>");
+	});
+}
