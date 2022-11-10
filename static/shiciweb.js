befdc3060812041caf78a23b1e4ff212271e17d4
ruanqizhen <ruanqizhen@gmail.com>
2022-11-09 19:53:54 -0500

    update

--- static/shiciweb.js
+++ static/shiciweb.js
@@ -64,8 +64,27 @@ function generateImage(){
 	}).fail(function (xhr, status) {
 		$('#result_section').html('<b>服务器目前无法工作。服务器是我的测试机器，不太稳定。请明天再试试！</b><br /><br />');
 	});
+	
+	var jqxhr2 = $.post(
+		"https://service.qizhen.xyz/poem/", 
+		$('#input_form').serialize(),
+	).done(function (data) {
+	    var result_div = $('#poem_result');
+		var ul = document.createElement('ul');
+		result_div.append(ul);
+		for(let i = 0; i < data.length; i++) {
+			var ps = document.createElement('li');
+			ps.innerHTML = '<span>' + data[i] + '</span>';
+			ul.append(ps);
+		}
+	}).fail(function (xhr, status) {
+		$('#poem_result').html('暂时没有诗词结果<br /><br />');
+	});
+	
+	
 	$('#add_comment').attr("style", "visibility: hidden")
-	$('#result_section').html('程序运行需要几十秒钟，请稍等 ……  <br> <progress style=\"width: 20%; height: 30px\"></progress>');
+	$('#poem_result').html('程序作诗需要几十秒钟，请稍等 ……');
+	$('#result_section').html('程序绘图需要几十秒钟，请稍等 ……  <br> <progress style=\"width: 20%; height: 30px\"></progress>');
 }
 
 function checkStatus(){
@@ -76,12 +95,12 @@ function checkStatus(){
 		var anchor_node = $('#model');
 		switch(data) {
 		  case "1":
-			anchor_node.html("目前正在运行<b>“吟诗作对”</b>模型。");
-			$('#genimg_link').addClass("disableHref");
+			// anchor_node.html("目前正在运行<b>“吟诗作对”</b>模型。");
+			// $('#genimg_link').addClass("disableHref");
 			break;
 		  case "2":
-			anchor_node.html("目前正在运行<b>“电脑绘图”</b>模型。");
-			$('#genpoem_link').addClass("disableHref");
+			// anchor_node.html("目前正在运行<b>“电脑绘图”</b>模型。");
+			// $('#genpoem_link').addClass("disableHref");
 			break;
 		  default:
 			anchor_node.html("<b>目前服务器无法工作，啥都运行不了！</b>");
@@ -95,6 +114,7 @@ function checkStatus(){
 		$('#genimg_link').addClass("disableHref");
 		$('#genpoem_link').addClass("disableHref");
 	});
+	document.querySelector('#submit_button').focus();
 }
 
 function addComments(){
@@ -103,6 +123,7 @@ function addComments(){
 	var image_node = $('#result_img');
 	comment_node.val($('#prompt').val() + '\n' + '<img src="' + image_node[0].src + '" />');
 	document.querySelector('#veditor').focus();
-	document.querySelector('#vcomments').focus();
-	document.querySelector('#input_form').focus();
+	document.querySelector('#submit_button').focus();
+	document.querySelector('#veditor').focus();
+	
 }
