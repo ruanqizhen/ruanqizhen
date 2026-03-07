5ed2faa0eabfb0ddeb1de809507bad0dc81f58f9
ruanqizhen <ruanqizhen@gmail.com>
2026-03-07 09:47:41 -0500

    update

--- tank/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { GameManager } from './engine/GameManager';
-
-const canvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
-
-const gameManager = new GameManager(canvas);
-gameManager.start();
-
-console.log('GameManager started.');
